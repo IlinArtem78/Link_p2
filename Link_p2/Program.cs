9b679e03a9d61ba2a705781633c2b018d9a848d5
IlinArtem78 <artem.ilin1994@gmail.com>
2024-09-07 11:50:23 +0300

    Добавьте файлы проекта.

---
+++ Link_p2/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        long t = Factorial(10); 
+        Console.WriteLine("Результат равен {0}", t);
+    }
+
+
+    static long Factorial(int number)
+    {
+        // Ваш код здесь
+        if (number == 0)
+        {
+            long result = 0;
+        }
+        else{
+            var numbers = new List<int>();
+
+            // Добавляем все числа от 1 до n в коллекцию
+            for (int i = 1; i <= number; i++)
+                numbers.Add(i);
+
+
+            result = numbers.Aggregate((x, y) => x * y);
+
+        }
+        return result;
+    }
+}
+
+
+
+
+
+
